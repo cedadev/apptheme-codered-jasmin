42e131c04fb1fba7f94d7e87cb1146d5455415f7
mjpritchard <matt.pritchard@stfc.ac.uk>
2020-04-30 17:10:50 +0100

    updated install_requires dependencies

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ if __name__ == "__main__":
         packages = find_packages(),
         include_package_data = True,
         zip_safe = False,
-        install_requires = ['coderedcms',],
+        install_requires = ['coderedcms','django-feedparser'],
         #extras_require = { },
     )
